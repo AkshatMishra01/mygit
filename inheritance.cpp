4a77d1e6e192b3ffbaea565c6993b3eb2425631c
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-02-26 12:43:26 +0530

    inheritance

---
+++ inheritance.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class parent
+{
+int par_id;
+public:
+//this is the parent class from which the characteristics have to be inherited
+father()
+{
+cout<<"i am the father function";
+}
+};
+class child : public child 
+int main()
+{
+child obj1;
+obj1.father();
+return 0;
+}
