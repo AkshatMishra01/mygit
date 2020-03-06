d986f376cf2090cfb172ca9f4d65f0c643f9637e
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-03-06 21:34:02 +0530

    Create git.cpp

---
+++ git.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class juju
+{
+int a,b,c;
+public:
+void fun(int x)
+{
+cout<<"this is the void function";
+}
+};
+int main()
+{
+juju obj;
+obj.fun();
+return 0;
+}
