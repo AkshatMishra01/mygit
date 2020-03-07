e1499125e439c0ba3c2e17c6b00cc79d3d41bb4c
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-03-07 14:56:43 +0530

    cocatenation

---
+++ cocate.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+string first = "Akshat kumar ";
+string second = "Mishra";
+string fullname= first + second;
+cout<<fullname;
+return 0;
+}
