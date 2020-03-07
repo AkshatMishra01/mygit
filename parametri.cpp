ae8aa38c8ff6dba44441868c097c6c88c2e1bf16
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-03-07 17:41:17 +0530

    UD

---
+++ parametri.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class consta
+{
+int a,b,c;
+public:
+consta(int x,int y)
+{
+cout<<"this is the  class";
+}
+};
+int main()
+{
+consta obj(4,7);
+return 0;
+}
