78bccbdf41c0ead4a387f4fd1f27bdec1de11402
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-03-20 09:49:13 +0530

    while loop

---
+++ while.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+int fact;a,b,c;
+cout<<"hello user\nthis is a factorial programme\nenter the number";
+cin>>a;
+while(i=0;i<fact)
+{
+fact=fact*i;
+i++;
+}
+cout<<fact;
+return 0;
+}
