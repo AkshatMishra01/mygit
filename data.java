fdeddef42ccbb9145a852e5916e54d3692b48fb5
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-13 18:10:51 +0530

    Create data.java

---
+++ data.java
@@ -0,0 +1,11 @@
+class CharExample {
+    public static void main(String[] args) {
+    	
+        char letter1 = '9';
+        System.out.println(letter1);
+    	
+        char letter2 = 65;
+        System.out.println(letter2);
+
+    }
+}
