6c0011f297761cac29d1841dc66b7a489e1be254
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-14 11:23:49 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,14 @@
+class ArrayExample {
+    public static void main(String[] args) {
+    
+        // create an array of length 5  
+        int[] age = new int[5];
+     
+        // access each element of the array using the index number 
+        System.out.println(age[0]);
+        System.out.println(age[1]);
+        System.out.println(age[2]);
+        System.out.println(age[3]);
+        System.out.println(age[4]);
+    }
+}
