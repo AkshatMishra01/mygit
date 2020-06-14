753bb5ee141ef6644f1a415c49a03825f53f96d7
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-14 10:38:45 +0530

    input in java

---
+++ javainput.java
@@ -0,0 +1,20 @@
+import java.util.scanner;
+class main
+{
+public static void main(String[] args)
+{
+float dataType;
+System.out.println("This is the infinite loop programme");
+System.out.println("Enter the numbers");
+Scanner input = new Scanner(System.in);
+switch(dataType)
+{
+case 2.4:
+System.out.println("hello world");
+break;
+case 5.7:
+System.out.println("is this even correct whatever the fuck i iam typing??");
+btreak;
+}
+}
+}
